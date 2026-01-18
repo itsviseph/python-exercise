36125b657f2bebf5b8422a8fa630c26670e5b062
itsviseph <vish12jos2000@gmail.com>
2026-01-18 18:00:15 +0530

    Add cart item counter exercise

---
+++ cart_item_counter.py
@@ -0,0 +1,11 @@
+# Exercise: Cart Item Counter
+# Count how many items are in the cart
+
+my_list = ('apple', 'banana', 'mango', 'pineapple', 'orange', 'grapes')
+
+counter = 0
+
+for item in my_list:
+    counter += 1
+
+print(counter)
