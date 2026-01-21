6f8e6fb65e35a4d4e8cdc5595e972d47ae01dee3
itsviseph <vish12jos2000@gmail.com>
2026-01-21 17:43:11 +0530

    update PEP8

--- logical_operators.py
+++ logical_operators.py
@@ -3,12 +3,12 @@ is_pro = True
 
 # checking if swimmer AND pro - "you are an elite athlete"
 if is_swimmer and is_pro:
-    print('you are an elite athlete')
+    print("you are an elite athlete")
 
 #  checking if swimmer but not expert "you need to practice more"
 elif is_swimmer and not is_pro:
-    print('you need to practice more') 
+    print("you need to practice more")
 
 # if not swimmer "you need fins"
 if not is_swimmer:
-    print('you need fins')
+    print("you need fins")
