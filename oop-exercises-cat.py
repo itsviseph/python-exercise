beff98bd971244099f797662e0389291a7e987c7
itsviseph <vish12jos2000@gmail.com>
2026-01-22 16:05:14 +0530

    exercise complete

--- oop-exercises-cat.py
+++ oop-exercises-cat.py
@@ -16,3 +16,51 @@ class Cat:
             print(f"{self._name} has played. Current energy is {self._energy}")
         else:
             print(f"{self._name} is too tired to play. Energy is {self._energy}")
+
+    def sleep(self):
+        self._energy = 100
+        print(f"{self._name} had a nap. Energy is restored to {self._energy}")
+
+
+class LazyCat(Cat):
+    def play(self):
+        if self._energy >= 10:
+            self._energy -= 10
+            print(f"{self._name} has played lazily. Current energy is {self._energy}")
+        else:
+            print(f"{self._name} is too tired to play. Energy is {self._energy}")
+
+
+class ActiveCat(Cat):
+    def play(self):
+        if self._energy >= 30:
+            self._energy -= 30
+            print(f"{self._name} has played Actively. Current energy is {self._energy}")
+        else:
+            print(f"{self._name} is too tired to play. Energy is {self._energy}")
+
+
+class CatManager:
+    def __init__(self, cats):
+        self.cats = cats
+
+    def play_all(self):
+        for cat in self.cats:
+            cat.play()
+
+    def eat_all(self):
+        for cat in self.cats:
+            cat.eat()
+
+    def sleep_all(self):
+        for cat in self.cats:
+            cat.sleep()
+
+
+cats = [Cat("Oscar"), LazyCat("Milo"), ActiveCat("Leo")]
+
+manager = CatManager(cats)
+
+manager.play_all()
+manager.eat_all()
+manager.sleep_all()
